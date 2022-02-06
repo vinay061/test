fd8211e2cc16c4f275e7836285aa4d88f986b751
wavy health <macos@wavys-MBP.fritz.box>
2022-02-06 18:58:25 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
 
-  let p = 0
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {
-          
-          <div>
-            <p>The value of p is: {p}</p>
-          </div>
-        }
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-3xl font-bold underline text-center">
+        Hello world!
+      </h1>
     </div>
   );
 }
