2bc392835770d644a885a6fa4133232c7e568bf5
wavy health <macos@wavys-MBP.fritz.box>
2022-02-06 17:42:08 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  let p = 0
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        {
+          
+          <div>
+            <p>The value of p is: {p}</p>
+          </div>
+        }
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
